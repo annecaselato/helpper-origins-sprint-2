09e17f96ab741bfa1c19aa617b72ac9812d0a23f
annecaselato <88508714+annecaselato@users.noreply.github.com>
2021-11-05 10:18:33 -0300

    Create VowelCounter.js

---
+++ exercicio-5/VowelCounter.js
@@ -0,0 +1,14 @@
+var VowelCounter = /** @class */ (function () {
+    function VowelCounter() {
+    }
+    VowelCounter.countVowels = function (word) {
+        var _this = this;
+        var wordArray = word.toLowerCase().split("");
+        return wordArray.filter(function (letter) { return _this.vowels.includes(letter); }).length;
+    };
+    VowelCounter.vowels = 'aeiou';
+    return VowelCounter;
+}());
+//const promptedWord: string = (<HTMLInputElement>document.getElementById("fword")).value;
+//document.getElementById("answer")!.innerHTML = VowelCounter.countVowels(promptedWord).toString();
+//console.log(VowelCounter.countVowels('tEste'));

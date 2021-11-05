7d9d2ba4b0500b5164e65013369ed6c617610f1f
annecaselato <88508714+annecaselato@users.noreply.github.com>
2021-11-05 10:41:08 -0300

    Update Person.ts

---
+++ exercicio-6/Person.ts
@@ -0,0 +1,11 @@
+class Person {
+    id: number;
+    name: string;
+    bio: string;
+
+    constructor (id: number, name: string, bio: string) {
+        this.id = id;
+        this.name = name;
+        this.bio = bio;
+    }
+}

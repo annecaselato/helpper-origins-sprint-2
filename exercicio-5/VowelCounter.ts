4fd8d70710679de89cc7f2f0110c0fb5682ec7ba
annecaselato <88508714+annecaselato@users.noreply.github.com>
2021-11-05 09:37:40 -0300

    Update VowelCounter.ts

--- exercicio-5/VowelCounter.ts
+++ exercicio-5/VowelCounter.ts
@@ -1,30 +1,11 @@
 class VowelCounter {
-    word: string;
-    readonly vowels: string;
+    static vowels: string = 'aeiou';
+    
 
-    constructor(word: string, vowels: string) {
-        this.word = word;
-        this.vowels = 'aeiou';
+    static countVowels(word: string): number {
+        let wordArray: string[] = word.toLowerCase().split("");
+        return wordArray.filter(letter => this.vowels.includes(letter)).length;
     }
-
-countVowels(word: string): number {
-    let wordArray: string[] = this.word.toLowerCase().split("");
-    let vowelCount: number = wordArray.filter(letter => this.vowels.includes(letter)).length;
-    return vowelCount;
-}
-/*const vowels: string = 'aeiou';
-
-//Função que retorna a quantidade de vogais da palavra passada
-function countVowels(word: string) {
-    word = word.toLowerCase();
-    let wordArray: string[] = word.split("");
-    let vowelCount: number = wordArray.filter(letter => vowels.includes(letter)).length;
-    return vowelCount;
 }
 
-//Função para processar o input do formulário e mostrar o resultado
-function formResult() {
-    const promptedWord: string = (<HTMLInputElement>document.getElementById("word")).value;
-    document.getElementById("result")!.innerHTML = countVowels(promptedWord).toString();
-}*/
-}
+console.log(VowelCounter.countVowels('Caselato'));

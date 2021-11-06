ca52fd29e68126c634df9e0b9a59e5c07a1ac15d
annecaselato <88508714+annecaselato@users.noreply.github.com>
2021-11-06 10:21:43 -0300

    Create ImperativeListInspector.ts

---
+++ ImperativeListInspector.ts
@@ -0,0 +1,34 @@
+//This class receives a list, check if it's numeric, and returns its max, min, and avarage values
+class ImperativeListInspector {
+    static inspectList(inputList: any[]): number[] {
+        //Check if the list is numeric and convert numeric strings to numbers
+        for (let index = 0; index < inputList.length; index++) {
+            if (isNaN(+inputList[index])) {
+                inputList.splice(index, 1);
+            }
+            inputList[index] = +inputList[index];
+        }
+
+        let maxValue: number = inputList[0];
+        let minValue: number = inputList[0];
+        let sum: number = 0;
+    
+        //Finds the max and min values, and sum all values
+        for (let index = 0; index < inputList.length; index++) {
+            sum += inputList[index];
+            if (inputList[index] < minValue) {
+                minValue = inputList[index];
+            }
+            if (inputList[index] > maxValue) {
+                maxValue = inputList[index];
+            }  
+        }
+    
+        //Calculates the avarage value
+        let avgValue: number = parseFloat((sum/inputList.length).toFixed(2));
+    
+        return [maxValue, minValue, avgValue];
+    }
+}
+
+console.log

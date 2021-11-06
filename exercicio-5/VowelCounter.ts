3aa1ecc8cd5c10b56c6a30d444c8252180f3df26
annecaselato <88508714+annecaselato@users.noreply.github.com>
2021-11-06 09:14:32 -0300

    Update VowelCounter.ts

--- exercicio-5/VowelCounter.ts
+++ exercicio-5/VowelCounter.ts
@@ -1,14 +1,15 @@
+//This class receives a word and returns its number of vowels
 class VowelCounter {
     static vowels: string = 'aeiou';
 
     static countVowels(word: string): number {
         let wordArray: string[] = word.toLowerCase().split("");
-        let wordVowels: string[]  = wordArray.filter(letter => this.vowels.includes(letter));
+        let wordVowels: string[] = wordArray.filter(letter => this.vowels.includes(letter));
         return wordVowels.length;
     }
 }
 
-//This Function get a input word from a form, clear the form field, and shows the answer on the screen
+//This Function gets a input word from a form, clears the form field, and shows the answer on the screen
 function formResult() {
     const givenWord: string = (<HTMLInputElement>document.querySelector("#input-word")).value;
     const answer: number = VowelCounter.countVowels(givenWord);

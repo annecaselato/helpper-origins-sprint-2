8db9bf02edaeb97f25ef7c9741fa5f941fa9ea53
annecaselato <88508714+annecaselato@users.noreply.github.com>
2021-11-06 23:58:09 -0300

    Update ImperativeListInspector.ts

--- exercicio-7/ImperativeListInspector.ts
+++ exercicio-7/ImperativeListInspector.ts
@@ -1,31 +1,42 @@
 //This class receives a list, check if it's numeric, and returns its max, min, and avarage values
 class ImperativeList {
-    static inspectList(inputList: any[]): number[] {
-        //Check if the list is numeric and convert numeric strings to numbers
-        for (let index = 0; index < inputList.length; index++) {
-            if (isNaN(+inputList[index])) {
-                inputList.splice(index, 1);
-            }
-            inputList[index] = +inputList[index];
-        }
+    list : any[];
 
-        let maxValue: number = inputList[0];
-        let minValue: number = inputList[0];
+    constructor(list: any[]) {
+        this.list = list;
+    }
+
+    //Check if it's numeric and convert numeric strings to numbers
+    private numericList(): number[] {
+        for (let index = 0; index < this.list.length; index++) {
+            if (isNaN(+this.list[index])) {
+                this.list.splice(index, 1);
+            }
+            this.list[index] = +this.list[index];
+        }
+        return this.list;
+    }
+
+    //Gets max, min and avarage values
+    public inspectList(): number[] {
+        let numericList: number[] = this.numericList();
+        let maxValue: number = numericList[0];
+        let minValue: number = numericList[0];
         let sum: number = 0;
     
         //Finds the max and min values, and sum all values
-        for (let index = 0; index < inputList.length; index++) {
-            sum += inputList[index];
-            if (inputList[index] < minValue) {
-                minValue = inputList[index];
+        for (let index = 0; index < numericList.length; index++) {
+            sum += numericList[index];
+            if (numericList[index] < minValue) {
+                minValue = numericList[index];
             }
-            if (inputList[index] > maxValue) {
-                maxValue = inputList[index];
+            if (numericList[index] > maxValue) {
+                maxValue = numericList[index];
             }  
         }
     
         //Calculates the avarage value
-        let avgValue: number = parseFloat((sum/inputList.length).toFixed(2));
+        let avgValue: number = parseFloat((sum/numericList.length).toFixed(2));
     
         return [maxValue, minValue, avgValue];
     }

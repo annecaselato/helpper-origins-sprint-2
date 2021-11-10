e6e2f7f7eac7f0937fe6315273c51669c700f3aa
annecaselato <annecaselato@gmail.com>
2021-11-10 19:59:27 -0300

    Update FunctionalListInspector.ts

--- exercicio-7/FunctionalListInspector.ts
+++ exercicio-7/FunctionalListInspector.ts
@@ -1,17 +1,38 @@
-//This class receives a list, check if it's numeric, and returns its max, min, and avarage values
+/**
+ * FunctionalList
+ * 
+ * This class represents a list and contain methods to inspect this list using the Functional programming paradigm
+ */
 class FunctionalList {
     private list : any[];
 
+    /**
+     * class constructor
+     * 
+     * @param list - is an array of type any
+     */
     constructor(list: any[]) {
         this.list = list;
     }
 
-    //Check if it's numeric and convert numeric strings to numbers
+    /**
+    * numericList
+    * 
+    * This method convert numeric strings to numbers and remove non-numeric values
+    * 
+    * @returns an numeric list
+    */
     private numericList(): number[] {
         return this.list.map(Number).filter(Boolean);
     }
 
-    //Gets max, min and avarage values
+    /**
+    * inspectList
+    * 
+    * This method finds the max, min and avarage values of the numeric list
+    * 
+    * @returns an array with the max, min and avarage numeric values
+    */
     public inspectList(): number[] {
         let numericList: number[] = this.numericList();
         let maxValue: number = Math.max(...numericList);
@@ -23,6 +44,6 @@ class FunctionalList {
     }
 }
 
-//Use example
+//Usage example
 let functionalExample = new FunctionalList([1,2,"cat","3"]);
 console.log(functionalExample.inspectList());
